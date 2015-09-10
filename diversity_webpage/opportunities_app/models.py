00f4976a769f7bfd1f4f8ef30b4204b308144e15
Kevin Klaes <kevin.klaes@gmail.com>
2015-09-10 11:11:23 -0500

    Models updates including enforcement of fields+clarification.

--- diversity_webpage/opportunities_app/models.py
+++ diversity_webpage/opportunities_app/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class Post(models.Model):
-	created_at = models.DateTimeField(auto_now_add=True)
-	name = models.CharField(max_length=100)
-	address = models.CharField(max_length=100)
-	point_of_contact = models.CharField(max_length=100)
-	phone = models.IntegerField(default=0)
-	email = models.CharField(max_length=100)
-	content = models.TextField()
 
-	def __unicode__(self):
-		return self.title
-	
-# Its the shape of the data as it is going into the database, and coming from the databse . that is a model. 
+class Post(models.Model):
+  """
+  A job posting.
+  """
+  created_at = models.DateTimeField(auto_now_add=True)
+  last_updated = models.DateTimeField(auto_now=True)
+
+  email = models.EmailField()
+  address = models.CharField(max_length=100, blank=True, null=True,
+    help_text='Address of the business')
+  point_of_contact = models.CharField(max_length=100,
+    help_text='Posting contact point')
+
+  name = models.CharField(max_length=100, help_text='Title of the job post')
+  content = models.TextField()
+
+  def __unicode__(self):
+    return self.title
+
+# Its the shape of the data as it is going into the database, and coming from the databse . that is a model.
+
